0c13eb812af1047a0dae5c9c8949f9c8951c393f
Renato Oliveira <renatog.oliveira17@gmail.com>
2025-05-02 14:11:19 -0300

    project set up

---
+++ src/main/java/com/renato/projects/hora_no_servidor/HoraNoServidorApplication.java
@@ -0,0 +1,13 @@
+package com.renato.projects.hora_no_servidor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoraNoServidorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoraNoServidorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/renato/projects/hora_no_servidor/HoraNoServidorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.renato.projects.hora_no_servidor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoraNoServidorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
